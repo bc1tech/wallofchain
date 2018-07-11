dbe825da02374c6b7801ae2a18fa96cfcb5dae26
Pedro Di Martino <dimartino.pedro@gmail.com>
2018-07-11 15:59:24 +0200

    fix deploy script and add to npm script

--- dev/deploy.sh
+++ dev/deploy.sh
@@ -141,7 +141,7 @@ main() {
 initial_deploy() {
 	git --work-tree "$deploy_directory" checkout --orphan $deploy_branch
 	git --work-tree "$deploy_directory" add --all
-	commit+push
+	commit_push
 }
 
 incremental_deploy() {
@@ -156,7 +156,7 @@ incremental_deploy() {
 	set -o errexit
 	case $diff in
 		0) echo No changes to files in $deploy_directory. Skipping commit.;;
-		1) commit+push;;
+		1) commit_push;;
 		*)
 			echo git diff exited with code $diff. Aborting. Staying on branch $deploy_branch so you can debug. To switch back to master, use: git symbolic-ref HEAD refs/heads/master && git reset --mixed >&2
 			return $diff
@@ -164,7 +164,7 @@ incremental_deploy() {
 	esac
 }
 
-commit+push() {
+commit_push() {
 	set_user_id
 	git --work-tree "$deploy_directory" commit -m "$commit_message"
 
@@ -208,14 +208,6 @@ restore_head() {
 	fi
 
 	git reset --mixed
-}
-
-filter() {
-	sed -e "s|$repo|\$repo|g"
-}
-
-sanitize() {
-	"$@" 2> >(filter 1>&2) | filter
 }
 
 [[ $1 = --source-only ]] || main "$@"
