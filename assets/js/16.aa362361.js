52db4991fb7538898f717617b443a532a1657d30
vittominacori <vittoriominacori@gmail.com>
2018-12-21 15:32:17 +0100

    publish: feat(header): main menu
    generated from commit 90018e907475744a34554c156615ddb80192a66f

---
+++ assets/js/16.aa362361.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{193:function(t,e,n){"use strict";n.r(e);var s=n(2),a=Object(s.a)({},function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"content"})},[],!1,null,null,null);a.options.__file="become-a-star.md";e.default=a.exports}}]);
---
+++ assets/js/19.9e7e3525.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{189:function(t,n,e){"use strict";e.r(n);var s=e(2),i=Object(s.a)({},function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"content"})},[],!1,null,null,null);i.options.__file="star.md";n.default=i.exports}}]);
